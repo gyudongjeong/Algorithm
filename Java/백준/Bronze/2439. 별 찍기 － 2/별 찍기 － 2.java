56876efdf0f3bdbe21fa691621a64286fca5b977
gyudongjeong <121234206+gyudongjeong@users.noreply.github.com>
2026-04-08 17:08:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 288 ms, Memory: 18288 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+        scan.close();
+		
+        for (int col = 1; col <= N; col++) {
+            for (int spa = 1; spa <= N-col; spa++) {
+            	System.out.print(" ");
+            }
+            
+            for (int sta = 1; sta <= col; sta++) {
+            	System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
